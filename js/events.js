bd6b8811fd512798fd2688d16f04a0f03e4343fd
Paris Dolfman <parisdolfman@gmail.com>
2019-01-18 18:09:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,19 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt() {
+ // bind a click event to the p  tag that alerts "Hey!"
+}
+
+function frameIt() {
+  // bind load event that adds class 'tasty' to the image to add a red frame around the image
+}
+
+function pressIt() {
+  // bind a keydown event to the input field of the form that alerts a user when they've pressed the 'g' key.
+}
+
+function submitIt() {
+  // bind a submit event to the form that alerts "Your form is going to be submitted now.".
+}
